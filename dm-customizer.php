d78c00502f4bde5b134199321096f4f0b3832260
Jay <jatacid@gmail.com>
2016-02-28 02:22:59 +1000

    Commit
    commit

--- dm-customizer.php
+++ dm-customizer.php
@@ -39,8 +39,11 @@ function dentex_register_theme_customizer( $wp_customize ) {
 
     $wp_customize->add_setting( 'dentex_media_css', array(
       'default' => '@media (min-width: 768px){}
+
 @media (min-width: 992px){}
+
 @media (min-width: 1100px){}
+
 @media (min-width: 1200px){}',
       )
     );
